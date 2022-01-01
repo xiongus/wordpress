9ae043f247f964507947af353aa6058f70382008
xiongus <guisheng.xiong@foxmail.com>
2022-01-01 22:05:39 +0800

    change file path

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -3,6 +3,6 @@
 COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
-cd /home/sammy/wordpress/
+cd /root/wordpress/
 $COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
